7afb0b2db79ea7480154bb630f70b1dcb5824e7f
roy <kimathiwaweru@gmail.com>
2022-11-27 13:46:44 +0300

    inviting participants from the group

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-staircase = (num) => {
+//@author Group2-phoenix 
+
+
+
+staircase = (num) => {       //define arrow function 'staircase' that console logs a number of steps depending on the 'num' passed in as an argument. 
+
 
     
-    let step = "#";
+    let step = "#";  // define step variable to represent a string of character '#'
     
-    for (let count = 0; count<=num; count++){
-        console.log(step +"\n");
-        step = step + "#";
+    for (let count = 0; count<=num; count++){      //create a for loop: as long as counter 'count' is equal to or less than the 'num' of steps  
+        console.log(step +"\n");    //console.log 'step' hence prints "#" string
+        step = step + "#"; //concatinate "#"to string 'step'
 
+
+        
+
+
+        
+
+        
     }
+    return num;
 
 
 
 }
 
-console.log(staircase(7)); 
+console.log(staircase(30)); 
+
+
+
+// manipulateDom = () => {
+//     let rootDiv = document.getElementById('root').appendChild(stairsForm).appendChild(enterNum);
+//     rootDiv.style.height = "80%";
+//     rootDiv.style.width = "80%";
+//     rootDiv.style.textAlign = "center";
+
+//     let stairsForm = document.createElement('form');
+//     stairsForm.setAttribute('id', "stairsForm");
+//     stairsForm.style.margin = '10px';
+//     stairsForm.style.padding = '10px';
+//     stairsForm.style.color = 'white';
+//     stairsForm.style.backgroundcolor = "grey";
+
+    
+
+    
+  
+
+
+//     let enterNum = document.createElement("TEXTAREA").value = "enter the number of steps";
+//     let submit = document.createElement("INPUT");
+//     submit.setAttribute("type", "submit");
+    
+
+    
+//     stairsForm.appendChild(submit);
+
+
+
+    
+    
+    
+
+
+
+}
+
+manipulateDom();
