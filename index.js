cdb8200cab95060a4e57a6155f9bae77c9971384
roy <kimathiwaweru@gmail.com>
2022-11-27 08:33:35 +0300

    updated js file, assignment scope covered

---
+++ index.js
@@ -0,0 +1,16 @@
+staircase = (num) => {
+
+    
+    let step = "#";
+    
+    for (let count = 0; count<=num; count++){
+        console.log(step +"\n");
+        step = step + "#";
+
+    }
+
+
+
+}
+
+console.log(staircase(7)); 
